ff91a2eb87a095e6c77ab11de2fdcc34021ce4de
sathishkumaravelu <sathishkumaravelu@gmail.com>
2023-11-09 23:11:10 +0530

    Dsa Problem solving

---
+++ src/main/java/org/sdet5/arrays/LargestElementinArray.java
@@ -0,0 +1,48 @@
+package org.sdet5.arrays;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LargestElementinArray {
+
+    /*
+        input  - int array
+        output - int
+     */
+    @Test
+    public void positiveData(){
+        int[] input = {1,2,10,1,41,1};
+        Assert.assertEquals(findLargeValue(input),41);
+    }
+    @Test
+    public void edgeData(){
+        int[] input = {1,2,10,1,-41,-1};
+        Assert.assertEquals(findLargeValue(input),10);
+    }
+
+    @Test
+    public void edgeData1(){
+        int[] input = {};
+        Assert.assertEquals(findLargeValue(input),Integer.MIN_VALUE);
+    }
+
+    @Test
+    public void negativeData(){
+        int[] input = {-41,-1};
+        Assert.assertEquals(findLargeValue(input),-1);
+    }
+
+    /*
+        Pseudo Code:
+        1. Initalize the result variable with Integer.Min
+        2. Iterate through the given array
+            check if the value is greater than result then replace with it
+        3. return result
+     */
+
+    public int findLargeValue(int[] nums){
+        int result = Integer.MIN_VALUE;
+        for (int num:nums) if (num > result) result = num;
+        return result;
+    }
+}
