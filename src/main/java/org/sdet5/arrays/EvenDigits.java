97c5cc5ea2a6c152b9c7a28af2c1d1201586e8cb
sathishkumaravelu <sathishkumaravelu@gmail.com>
2023-12-02 23:57:06 +0530

    Dsa Problem solving

---
+++ src/main/java/org/sdet5/arrays/EvenDigits.java
@@ -0,0 +1,45 @@
+package org.sdet5.arrays;
+
+import org.testng.annotations.Test;
+
+public class EvenDigits {
+
+    @Test
+    public void testEvenDigit(){
+        int[] nums = {12,345,2,6,7896,11};
+        int noOfEven = getNoOfEven(nums);
+        System.out.println(noOfEven);
+    }
+
+    private int getNoOfEven(int[] nums) {
+        int count = 0;
+        for (int num: nums) {
+            if (even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean even(int num) {
+        int numberOfDigits = digits(num);
+        return numberOfDigits % 2 ==0;
+    }
+
+    private int digits(int num) {
+        // count number of digits in a number
+            if (num < 0) {
+                num = num * -1;
+            }
+            if (num == 0) {
+                return 1;
+            }
+            int count = 0;
+            while (num > 0) {
+                count++;
+                num = num / 10; // num /= 10
+            }
+            return count;
+        }
+
+    }
---
+++ src/main/java/org/sdet5/hashing/CountCharacter.java
@@ -0,0 +1,56 @@
+package org.sdet5.hashing;
+
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CountCharacter {
+
+    /*
+        count the number of character in the given string
+     */
+
+    @Test
+    public void testCharCount() {
+        String str = "Sathish";
+        findCharCount(str);
+        findCharCountCharArray(str);
+    }
+
+    //Time Complexity - O(N)
+    //Space Complexity - O(N)
+    private void findCharCount(String str) {
+        // to maintain the order of the value using the LinkedHashMap
+        Map<Character, Integer> frequencyMap = new LinkedHashMap<>();
+        for (char c : str.toCharArray()) {
+            frequencyMap.put(Character.toLowerCase(c), frequencyMap.getOrDefault(Character.toLowerCase(c), 0) + 1);
+        }
+        for (Map.Entry<Character, Integer> setVal : frequencyMap.entrySet()) {
+            System.out.print(setVal.getKey() + ": " + setVal.getValue()+" ");
+        }
+        System.out.println();
+    }
+
+    //Time Complexity - O(N)
+    //Space Complexity - O(1)
+    private void findCharCountCharArray(String str) {
+        // Assuming ASCII characters, you can adjust the size based on the character set
+        int[] frequencyArray = new int[128];
+
+        // Iterate through the characters of the string
+        for (char c : str.toCharArray()) {
+            // Update the frequency array for each character
+            frequencyArray[Character.toLowerCase(c)]++;
+        }
+
+        // Print the character frequencies
+        for (int i = 0; i < frequencyArray.length; i++) {
+            if (frequencyArray[i] > 0) {
+                System.out.print((char) i + ": " + frequencyArray[i]+ " ");
+            }
+        }
+        System.out.println();
+    }
+}
